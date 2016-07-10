72eaba16bfc3cc40e499dcdaefe97eeb8ba80859
karandash1908 <karandash1908@outlook.com>
2016-07-10 20:40:52 +0300

    Refactoring.

--- buying_blender.js
+++ buying_blender.js
@@ -3,11 +3,36 @@ var webdriver = require('..'),
     until = webdriver.until,
     chrome = require('../chrome');
 
-
 var driver = new webdriver.Builder()
     .forBrowser('chrome')
     .build();
+driver.manage().timeouts().implicitlyWait(10*1000);
 
+function executeJS(script) {
+    driver.executeScript(script).then(function(return_value) {
+    });
+}
+
+function scrollUp(){
+    executeJS("window.scrollTo(0,0);");
+    driver.sleep(1000);
+}
+
+function verifyValue(className, textForCheck, message) {
+    var element = driver.findElement(By.className(className));
+    element.getText().then(function (text) {
+        if (text = ! textForCheck) {
+            throw message + '  ERROR';
+        } else {
+            console.log(message + '  OK');
+        }
+    });
+}
+function waitForElement(className) {
+    driver.wait(function () {
+        return driver.findElement(By.className(className)).isDisplayed();
+    }, TimeOfLoading);
+}
 
 var TimeOfLoading = 10000;
 var TitleRozetka = 'Интернет-магазин ROZETKA™: фототехника, видеотехника, аудиотехника, компьютеры и компьютерные комплектующие';
@@ -16,75 +41,56 @@ var TitleBlenders = 'Блендеры - Интернет магазин Rozetka.
 var TitleTypeOfBlender = 'Тип: погружной. Блендеры - Интернет магазин Rozetka.ua | Купить Блендеры в Киеве: цена, отзывы, продажа.';
 var TitleOneBlender = 'Rozetka.ua | Блендер BRAUN MQ535 SAUCE. Цена, купить Блендер BRAUN MQ535 SAUCE в Киеве, Харькове, Днепропетровске, Одессе, Запорожье, Львове. Блендер BRAUN MQ535 SAUCE: обзор, описание, продажа.';
 
-function LogOutUser () {
-    if(driver.findElement(By.name('signin')) != null){
-        return;
-    }if(driver.findElement(By.name('profile')) != null){
-        driver.findElement(By.name('profile')).click();
-        driver.findElement(By.id('profile_signout')).click();
-        if(driver.findElement(By.name('signin')) != null){
-            return;
-        }else{
-            console.log('The user did not log out.')
-        }
-    }
-}
+//////////  1
 
 driver.get('http://www.rozetka.com.ua');
 driver.wait(until.titleIs(TitleRozetka), TimeOfLoading);
-LogOutUser;
-driver.findElement(By.linkText('Бытовая техника')).click();
+//////////  3
+
+driver.findElement(By.partialLinkText('Бытовая техника')).click();
 driver.wait(until.titleIs(TitleTechcnics),TimeOfLoading);
 driver.findElement(By.linkText('Блендеры')).click();
 driver.wait(until.titleIs(TitleBlenders), TimeOfLoading);
 driver.findElement(By.linkText('Погружной')).click();
 driver.wait(until.titleIs(TitleTypeOfBlender),TimeOfLoading);
 driver.findElement(By.linkText('Блендер BRAUN MQ535 SAUCE')).click();
-
 driver.wait(until.titleContains("Блендер BRAUN"), TimeOfLoading);
-driver.getTitle().then(function(text){
-    console.log("Title : " + text);
-});
+////////// 3.5
 
 driver.wait(until.titleIs(TitleOneBlender), TimeOfLoading);
-driver.findElement(By.className('detail-title')).then(function(){
-    if(text =! 'Блендер BRAUN MQ535 SAUCE'){
-        throw 'The required goods did not find.';
-    }
-});
-driver.findElement(By.className('detail-description')).then(function () {
-    if(text != 'Тип: погружной. Мощность: 600 Вт. Кол-во скоростей: 2. Мини-измельчитель: есть. Колка льда: нет. Материал ножки блендера: металл. Цвет: белый/серый.'){
-        throw 'The dicription of goods did not find.';
-    }
-});
+verifyValue('detail-title', 'Блендер BRAUN MQ535 SAUCE', 'The required goods have been found');
+verifyValue('detail-description', 'Тип: погружной. Мощность: 600 Вт. Кол-во скоростей: 2. Мини-измельчитель: есть.'+
+    'Колка льда: нет. Материал ножки блендера: металл. Цвет: белый/серый.', 'The description of goods have been found.');
+
+////////// 4
 
 driver.findElement(By.name('topurchases')).click();
-driver.wait(function() {
-    return driver.findElement(By.className('cart-title')).isDisplayed();
-}, TimeOfLoading);
-driver.findElement(By.className('cart-title')).then(function () {
-    if(text != 'Вы добавили товар в корзину'){
-        throw 'The goods did not added to cart';
-    }
-});
-driver.findElement(By.className('popup-close-icon sprite')).click();
+waitForElement('cart-title');
+verifyValue('cart-title', 'Вы добавили товар в корзину', 'The goods added to cart successfully.');
+scrollUp();
+driver.findElement(By.className('popup-close-icon')).click();
 
-driver.findElement(By.className('hub-i-count')).then(function () {
-    if(text != '1'){
-        throw 'The amount of goods did not show in cart.';
-    }
-});
+////////// 5
+verifyValue('hub-i-count', '1', 'The amount of goods have been shown in the cart.');
+driver.findElement(By.className('hub-i-cart-link-count')).click();
+waitForElement('cart-title');
+verifyValue('cart-i-title-link', 'Блендер BRAUN MQ535 SAUCE', 'The goods have been snown after reopen the cart.');
+scrollUp();
+driver.findElement(By.className('popup-close-icon')).click();
+
+////////// 6
+verifyValue('hub-i-count', '1', 'The amount of goods have been shown after reopen twice in the cart.');
 driver.findElement(By.className('sprite-side novisited hub-i-link hub-i-cart-link-count')).click();
-driver.wait(function () {
-    return driver.findElement(By.className('cart-title')).isDisplayed();
-}, TimeOfLoading);
-driver.findElement(By.name('goods-link')).then(function () {
-    if(text != 'Блендер BRAUN MQ535 SAUCE'){
-        throw 'The goods did not snow after reopen the cart.';
-    }
-});
-driver.findElement(By.name('close')).click();
+waitForElement('cart-title');
+driver.findElement(By.className('cart-check-icon')).click();
+driver.findElement(By.className('cart-i-delete-link')).click();
+verifyValue('empty-cart-title', 'Корзина пуста', 'The message have been shown about empty cart.');
+scrollUp();
+driver.findElement(By.className('popup-close-icon')).click();
 
-
+driver.findElement(By.className('hub-i-cart-link')).click();
+verifyValue('empty-cart-title', 'Корзина пуста', 'The innformation about empty cart have been shown.');
+scrollUp();
+driver.findElement(By.className('popup-close-icon')).click();
 
 driver.quit();
